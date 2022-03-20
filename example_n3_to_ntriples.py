7fb484fdb60beb8979f295161a31ed4f90520bd3
Kamal singh <kamal.singh25@gmail.com>
2022-03-20 12:29:17 +0100

    Update example_n3_to_ntriples.py

--- example_n3_to_ntriples.py
+++ example_n3_to_ntriples.py
@@ -4,12 +4,6 @@ from rdflib.namespace import FOAF, DC
 
 g2 = Graph()
 
-src2 = '''@prefix dbr: <http://dbpedia.org/resource/> .
-@prefix dbo: <http://dbpedia.org/ontology/> .
-
-dbr:France dbo:capital dbr:Paris .
-dbr:Paris dbo:populationTotal "2229621"^^xsd:integer .'''
-
 src = '''@prefix rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 [ a rdf:Statement ;
